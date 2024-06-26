d212f943adea5ad0eee034759a655360045636e8
mahursh <127805335+mahursh@users.noreply.github.com>
2024-06-27 03:49:13 +0430

    MessageSender added

---
+++ tabooGame/telegram/src/main/java/com/mftplus/game/telegramExecutor/message/MessageSender.java
@@ -0,0 +1,20 @@
+package com.mftplus.game.telegramExecutor.message;
+
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.DefaultAbsSender;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+@Component
+public class MessageSender extends DefaultAbsSender {
+    public MessageSender(@Value("${app.telegram.token}") String botToken) {
+        super(new DefaultBotOptions(), botToken);
+    }
+
+    @SneakyThrows
+    public Object sendMessage(SendMessage message){
+        return execute(message);
+    }
+}
