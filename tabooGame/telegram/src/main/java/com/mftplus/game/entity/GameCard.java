4ab93d1f9f72e14f04bbaacec71cad3f49881166
mahursh <127805335+mahursh@users.noreply.github.com>
2024-06-26 05:10:44 +0430

    GameCard entity added

---
+++ tabooGame/telegram/src/main/java/com/mftplus/game/entity/GameCard.java
@@ -0,0 +1,33 @@
+package com.mftplus.game.entity;
+
+import com.mftplus.game.enums.CardStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+
+@Entity(name = "gameCardEntity")
+@Table(name = "game_card_tbl")
+public class GameCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Card card;
+
+    @ManyToOne
+    private Game game;
+
+    @Enumerated(EnumType.STRING)
+    private CardStatus cardStatus = CardStatus.IN_PROGRESS;
+}
