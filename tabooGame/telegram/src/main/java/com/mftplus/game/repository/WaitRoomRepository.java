6bdddd2fbdde9228c83e7c922ee8e6dcd9edc716
mahursh <127805335+mahursh@users.noreply.github.com>
2024-06-27 22:46:36 +0430

    WaitRoomRepository Added

---
+++ tabooGame/telegram/src/main/java/com/mftplus/game/repository/WaitRoomRepository.java
@@ -0,0 +1,9 @@
+package com.mftplus.game.repository;
+
+import com.mftplus.game.entity.WaitRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WaitRoomRepository extends JpaRepository<WaitRoom , Long> {
+}
