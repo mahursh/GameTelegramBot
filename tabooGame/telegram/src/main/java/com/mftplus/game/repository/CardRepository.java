ee06efb58526ba839c79816223957399702512da
mahursh <127805335+mahursh@users.noreply.github.com>
2024-06-28 23:29:47 +0430

    CardRepository Updated

--- tabooGame/telegram/src/main/java/com/mftplus/game/repository/CardRepository.java
+++ tabooGame/telegram/src/main/java/com/mftplus/game/repository/CardRepository.java
@@ -3,8 +3,14 @@ package com.mftplus.game.repository;
 import com.mftplus.game.entity.Card;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card , Long> {
+
+    @Query("SELECT c.id FROM cardEntity c")
+    List<Long> getAllIds();
 }
