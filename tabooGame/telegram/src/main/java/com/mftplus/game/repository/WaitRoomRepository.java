fe565d22d3613647cbcbf0f9ec6080acca52e141
mahursh <127805335+mahursh@users.noreply.github.com>
2024-06-28 06:53:54 +0430

    How to Access Columns of a Related Object Within Another

--- tabooGame/telegram/src/main/java/com/mftplus/game/repository/WaitRoomRepository.java
+++ tabooGame/telegram/src/main/java/com/mftplus/game/repository/WaitRoomRepository.java
@@ -1,6 +1,7 @@
 package com.mftplus.game.repository;
 
 import com.mftplus.game.entity.WaitRoom;
+import com.mftplus.game.enums.WaitRoomStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,7 @@ import org.springframework.stereotype.Repository;
 public interface WaitRoomRepository extends JpaRepository<WaitRoom , Long> {
 
     WaitRoom findByHash(String hash);
+
+    //TODO: Thiiiis iissss hoooow  weeee dooo iiittttt !!!!!!  :)))))
+    WaitRoom findByChat_telegramChatIdAndStatus(Long telegramChatId , WaitRoomStatus status);
 }
