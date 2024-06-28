6e44301bc9c35b56388d801bf01c1e527bae1e44
mahursh <127805335+mahursh@users.noreply.github.com>
2024-06-28 06:54:58 +0430

    @EnableScheduling Added

--- tabooGame/telegram/src/main/java/com/mftplus/game/TelegramApplication.java
+++ tabooGame/telegram/src/main/java/com/mftplus/game/TelegramApplication.java
@@ -2,10 +2,11 @@ package com.mftplus.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootApplication
-
+@EnableScheduling
 public class TelegramApplication {
 
 	public static void main(String[] args) throws TelegramApiException {
