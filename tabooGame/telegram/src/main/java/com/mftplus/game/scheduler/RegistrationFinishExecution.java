26238f7361f25736a720387af406b57622a6d47f
mahursh <127805335+mahursh@users.noreply.github.com>
2024-06-28 07:26:07 +0430

    <Bug>: Query did not return a unique result: 2 results were returned for some reason in wait_room_tbl we have two rows added

--- tabooGame/telegram/src/main/java/com/mftplus/game/scheduler/RegistrationFinishExecution.java
+++ tabooGame/telegram/src/main/java/com/mftplus/game/scheduler/RegistrationFinishExecution.java
@@ -4,9 +4,12 @@ package com.mftplus.game.scheduler;
 import com.mftplus.game.entity.WaitRoom;
 import com.mftplus.game.enums.WaitRoomStatus;
 import com.mftplus.game.service.WaitRoomService;
+import com.mftplus.game.telegramExecutor.TabooBot;
 import com.mftplus.game.telegramExecutor.message.MessageBuilder;
 import com.mftplus.game.telegramExecutor.message.MessageSender;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -27,22 +30,34 @@ public class RegistrationFinishExecution {
     private final WaitRoomService waitRoomService;
     private final MessageBuilder messageBuilder;
     private final MessageSender messageSender;
-
+    private static final Logger logger = LoggerFactory.getLogger(TabooBot.class);
     @Lazy
     @Autowired
     private RegistrationFinishExecution self;
 
     public void scheduleRegistrationFinish(Long telegramId){
+
+        logger.warn("INSIDE scheduleRegistrationFinish METHOD  - START!");
         taskScheduler.schedule(
                 () -> self.registrationFinished(telegramId),
                 Instant.now().plus(1, ChronoUnit.MINUTES)
         );
+
+        logger.warn("INSIDE scheduleRegistrationFinish METHOD  - END!");
+
     }
 
     public void registrationFinished(Long telegramChatId){
+        logger.warn("INSIDE registrationFinished METHOD !");
 
+        logger.error("bef");
+        //TODO: this part seems to cause the error !
         WaitRoom waitRoom = waitRoomService.findAwaitingByChatId(telegramChatId);
+        logger.error("aft");
+
         if (waitRoom == null) return;
+        logger.info("waitRoom Founded: {}" , waitRoom.toString());
+
         if (waitRoom.getUsers().size() < 2){
             EditMessageText notEnoughUserMsg = messageBuilder.editNotEnoughUserMsg(waitRoom.getMessageId() , telegramChatId);
             messageSender.sendMessage(notEnoughUserMsg);
