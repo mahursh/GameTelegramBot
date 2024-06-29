d4036ace8f89010d31124cd0a2810ba073eaa200
mahursh <127805335+mahursh@users.noreply.github.com>
2024-06-29 04:48:03 +0430

    Some Methods Added

--- tabooGame/telegram/src/main/java/com/mftplus/game/service/GameService.java
+++ tabooGame/telegram/src/main/java/com/mftplus/game/service/GameService.java
@@ -1,10 +1,17 @@
 package com.mftplus.game.service;
 
 import com.mftplus.game.entity.*;
+import com.mftplus.game.enums.CardStatus;
 import com.mftplus.game.enums.GameRole;
+import com.mftplus.game.enums.GameStatus;
 import com.mftplus.game.repository.GameRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -56,5 +63,64 @@ public class GameService {
         var userGame = new UserGame();
         userGame.setUser(user);
         return userGame;
+    }
+
+    public Game findGameInProgress(Long telegramChatId){
+        return gameRepository.findByChat_telegramChatIdAndStatus(telegramChatId , GameStatus.IN_PROGRESS);
+    }
+
+    public GameCard getGameCardInProgress(Game game){
+        return game.getCards()
+                .stream()
+                .filter(gc -> gc.getStatus().equals(CardStatus.IN_PROGRESS))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public UserGame whoseMessage(Long telegramId , Game game){
+        return game.getUsers()
+                .stream()
+                .filter(ug -> ug.getUser().getTelegramId().equals(telegramId))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public Set<String> findTabooUsed(Card card ,String text){
+        return extractWords(text.toLowerCase())
+                .stream()
+                .filter(w -> card.getAllTaboos().contains(w))
+                .collect(Collectors.toSet());
+    }
+
+    private List<String> extractWords(String text){
+        return Arrays.stream(text.split("[^a-zA-Z-]+")).toList();
+    }
+
+    public boolean isGuessed(Card card , String text){
+        return extractWords(text.toLowerCase()).contains(card.getAnswer());
+    }
+
+    public void cardWasGuessed(GameCard gameCard , UserGame guesser){
+        guesser.setScore(guesser.getScore() + 1) ;
+        Game game = gameCard.getGame();
+        UserGame explainer = getUserGameExplainer(game);
+        explainer.setScore(explainer.getScore() + 1);
+        gameCard.setStatus(CardStatus.COMPLETED);
+    }
+
+    public UserGame getUserGameExplainer(Game game){
+        return  game.getUsers()
+                .stream()
+                .filter(ug -> ug.getGameRole() == GameRole.EXPLAINER)
+                .findFirst()
+                .orElseThrow();
+    }
+    public User getExplainer(Game game){
+        return game.getUsers()
+                .stream()
+                .filter(ug -> ug.getGameRole() == GameRole.EXPLAINER)
+                .findFirst()
+                .map(UserGame::getUser)
+                .orElseThrow();
     }
 }
