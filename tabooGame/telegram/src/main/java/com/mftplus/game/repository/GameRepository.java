fc8e6e84d500b1afe00525b60ad286c4fdb9829a
mahursh <127805335+mahursh@users.noreply.github.com>
2024-06-29 04:46:52 +0430

    findByChat_telegramChatIdAndStatus Added inside GameRepository

--- tabooGame/telegram/src/main/java/com/mftplus/game/repository/GameRepository.java
+++ tabooGame/telegram/src/main/java/com/mftplus/game/repository/GameRepository.java
@@ -1,11 +1,12 @@
 package com.mftplus.game.repository;
 
 import com.mftplus.game.entity.Game;
+import com.mftplus.game.enums.GameStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game , Long> {
 
-
+Game findByChat_telegramChatIdAndStatus(Long telegramChatId , GameStatus status);
 }
